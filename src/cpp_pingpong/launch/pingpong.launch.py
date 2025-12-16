d5836d2996b3450ef898a933ec5f49a3eb572152
ogi-y <ioshelg@gmail.com>
2025-12-16 23:54:40 +0900

    コメント

--- src/cpp_pingpong/launch/pingpong.launch.py
+++ src/cpp_pingpong/launch/pingpong.launch.py
@@ -6,14 +6,14 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
-    # 設定ファイルのパスを取得
+    # 設定ファイルのパス
     config = os.path.join(
         get_package_share_directory('cpp_pingpong'),
         'config',
         'params.yaml'
     )
 
-    # カメラソース指定引数
+    # カメラソース引数
     camera_source_arg = DeclareLaunchArgument(
         'source',          # 実行時に指定する名前
         default_value='0', # 指定なしの場合の値
@@ -44,7 +44,6 @@ def generate_launch_description():
         ),
         
         # 3. Python 視覚ノード (メイン: YOLO/カメラ)
-        # ※ここに source パラメータを渡します
         Node(
             package='py_pingpong',
             executable='vision_main',
@@ -56,7 +55,6 @@ def generate_launch_description():
         ),
 
         # 4. Python 視覚ノード (サブ: 年齢推定)
-        # ※独立して起動させます
         Node(
             package='py_pingpong',
             executable='vision_age',
